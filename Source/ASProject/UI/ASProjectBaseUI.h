bf6da7002ab619535e79798ea3753944219ab942
tsmolinski <tsmolinski3@gmail.com>
2025-06-04 14:48:09 +0200

    Added CommonUI plugin to project, changed GameViewportClientClass for CommonUI. Created BaseUI widget to manage layers. Added Rider user files to .gitignore file.

---
+++ Source/ASProject/UI/ASProjectBaseUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "ASProjectBaseUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASPROJECT_API UASProjectBaseUI : public UCommonUserWidget
+{
+	GENERATED_BODY()
+	
+};
